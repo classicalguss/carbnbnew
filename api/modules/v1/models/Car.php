e5700eb9f59c9a9c76172d7e904eb72cd0e7c07b
Ghassan Barghouti <barghouti_since88@hotmail.com>
2017-06-18 01:08:50 +0300

    Add new columns

--- api/modules/v1/models/Car.php
+++ api/modules/v1/models/Car.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace api\modules\v1\models;
+namespace app\models;
 
 use Yii;
-use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "car".
@@ -19,49 +18,72 @@ use yii\db\ActiveRecord;
  * @property string $insurance_tip
  * @property integer $owner_id
  * @property string $report
+ * @property string $country
+ * @property string $city
+ * @property string $maker
+ * @property string $model
+ * @property integer $is_featured
+ * @property string $year_model
+ * @property string $gear_type
+ * @property integer $number_of_doors
+ * @property integer $number_of_seats
+ * @property integer $gas
+ * @property string $type
  */
-class Car extends ActiveRecord
+class Car extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return 'car';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['cover_photo', 'price', 'address', 'description', 'milage_limitation', 'insurance_tip', '!owner_id', 'report','city','country','maker','model'], 'required'],
-            [['price', 'rent_it_now', 'milage_limitation', 'owner_id'], 'integer'],
-            [['created_at'], 'safe'],
-            [['description', 'report'], 'string'],
-            [['cover_photo', 'address', 'insurance_tip'], 'string', 'max' => 255],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'cover_photo' => 'Cover Photo',
-            'price' => 'Price',
-            'created_at' => 'Created At',
-            'rent_it_now' => 'Rent It Now',
-            'address' => 'Address',
-            'description' => 'Description',
-            'milage_limitation' => 'Milage Limitation',
-            'insurance_tip' => 'Insurance Tip',
-            'owner_id' => 'Owner ID',
-            'report' => 'Report',
-        		'maker' => 'Maker',
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'car';
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+				[['cover_photo', 'price', 'address', 'description', 'milage_limitation', 'insurance_tip', 'owner_id', 'report', 'country', 'city', 'maker', 'model', 'type'], 'required'],
+				[['price', 'rent_it_now', 'milage_limitation', 'owner_id', 'is_featured', 'number_of_doors', 'number_of_seats', 'gas'], 'integer'],
+				[['created_at', 'year_model'], 'safe'],
+				[['description', 'report'], 'string'],
+				[['cover_photo', 'address', 'insurance_tip', 'model'], 'string', 'max' => 255],
+				[['country', 'city', 'maker'], 'string', 'max' => 50],
+				[['gear_type', 'type'], 'string', 'max' => 30],
+		];
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+				'id' => 'ID',
+				'cover_photo' => 'Cover Photo',
+				'price' => 'Price',
+				'created_at' => 'Created At',
+				'rent_it_now' => 'Rent It Now',
+				'address' => 'Address',
+				'description' => 'Description',
+				'milage_limitation' => 'Milage Limitation',
+				'insurance_tip' => 'Insurance Tip',
+				'owner_id' => 'Owner ID',
+				'report' => 'Report',
+				'country' => 'Country',
+				'city' => 'City',
+				'maker' => 'Maker',
+				'model' => 'Model',
+				'is_featured' => 'Is Featured',
+				'year_model' => 'Year Model',
+				'gear_type' => 'Gear Type',
+				'number_of_doors' => 'Number Of Doors',
+				'number_of_seats' => 'Number Of Seats',
+				'gas' => 'Gas',
+				'type' => 'Type',
+		];
+	}
 }
--- api/modules/v1/models/CarSearch.php
+++ api/modules/v1/models/CarSearch.php
@@ -1,119 +1,88 @@
 <?php
 
-namespace api\modules\v1\models;
+namespace app\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use api\modules\v1\models\Car;
+use app\models\Car;
 
 /**
  * CarSearch represents the model behind the search form about `app\models\Car`.
  */
-class CarSearch extends Car {
+class CarSearch extends Car
+{
 	/**
 	 * @inheritdoc
 	 */
-	public function rules() {
-		return [ 
-				[ 
-						[ 
-								'id',
-								'price',
-								'rent_it_now',
-								'milage_limitation',
-								'owner_id',
-								'is_featured',
-						],
-						'integer' 
-				],
-				[ 
-						[ 
-								'cover_photo',
-								'created_at',
-								'address',
-								'description',
-								'insurance_tip',
-								'report',
-								'model',
-								'maker',
-								'city',
-						],
-						'safe' 
-				] 
+	public function rules()
+	{
+		return [
+				[['id', 'price', 'rent_it_now', 'milage_limitation', 'owner_id', 'is_featured', 'number_of_doors', 'number_of_seats', 'gas'], 'integer'],
+				[['cover_photo', 'created_at', 'address', 'description', 'insurance_tip', 'report', 'country', 'city', 'maker', 'model', 'year_model', 'gear_type', 'type'], 'safe'],
 		];
 	}
 	
 	/**
 	 * @inheritdoc
 	 */
-	public function scenarios() {
+	public function scenarios()
+	{
 		// bypass scenarios() implementation in the parent class
-		return Model::scenarios ();
+		return Model::scenarios();
 	}
 	
 	/**
 	 * Creates data provider instance with search query applied
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 *
 	 * @return ActiveDataProvider
 	 */
-	public function search($params) {
-		$query = Car::find ();
+	public function search($params)
+	{
+		$query = Car::find();
 		
 		// add conditions that should always apply here
 		
-		$dataProvider = new ActiveDataProvider ( [ 
-				'query' => $query 
-		] );
+		$dataProvider = new ActiveDataProvider([
+				'query' => $query,
+		]);
 		
-		$this->load ( $params,'' );
+		$this->load($params);
 		
-		if (! $this->validate ()) {
+		if (!$this->validate()) {
 			// uncomment the following line if you do not want to return any records when validation fails
 			// $query->where('0=1');
 			return $dataProvider;
 		}
 		
 		// grid filtering conditions
-		$query->andFilterWhere ( [ 
+		$query->andFilterWhere([
 				'id' => $this->id,
+				'price' => $this->price,
 				'created_at' => $this->created_at,
 				'rent_it_now' => $this->rent_it_now,
 				'milage_limitation' => $this->milage_limitation,
 				'owner_id' => $this->owner_id,
-				'is_featured'=> $this->is_featured,
-		] );
-		$query->andFilterWhere ( [ 
-				'like',
-				'cover_photo',
-				$this->cover_photo 
-		] )->andFilterWhere ( [ 
-				'like',
-				'address',
-				$this->address 
-		] )->andFilterWhere ( [ 
-				'like',
-				'description',
-				$this->description
-		] )->andFilterWhere ( [ 
-				'like',
-				'insurance_tip',
-				$this->insurance_tip
-		] )->andFilterWhere ( [ 
-				'like',
-				'report',
-				$this->report
-		] )->andFilterWhere ( [
-				'like',
-				'city',
-				$this->city
-		] )->andFilterWhere ( [
-				'like',
-				'maker',
-				$this->maker
-		] );
+				'is_featured' => $this->is_featured,
+				'year_model' => $this->year_model,
+				'number_of_doors' => $this->number_of_doors,
+				'number_of_seats' => $this->number_of_seats,
+				'gas' => $this->gas,
+		]);
+		
+		$query->andFilterWhere(['like', 'cover_photo', $this->cover_photo])
+		->andFilterWhere(['like', 'address', $this->address])
+		->andFilterWhere(['like', 'description', $this->description])
+		->andFilterWhere(['like', 'insurance_tip', $this->insurance_tip])
+		->andFilterWhere(['like', 'report', $this->report])
+		->andFilterWhere(['like', 'country', $this->country])
+		->andFilterWhere(['like', 'city', $this->city])
+		->andFilterWhere(['like', 'maker', $this->maker])
+		->andFilterWhere(['like', 'model', $this->model])
+		->andFilterWhere(['like', 'gear_type', $this->gear_type])
+		->andFilterWhere(['like', 'type', $this->type]);
 		
 		return $dataProvider;
 	}
