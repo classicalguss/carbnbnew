7cbd1eb2334e8d7a25d81e8f738b501c5ad7ee1f
Mahmoud-Abd-Alhadi <mahmoud.abdalhadi5@gmail.com>
2017-08-11 23:07:59 +0300

    Edit Images path on production

--- common/config/params.php
+++ common/config/params.php
@@ -3,6 +3,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-	'imagesFolder' => (YII_ENV_DEV ? 'http://localhost/workspace/carbnbnew/uploads/' : 'http://178.62.110.252/uploads/'),
-	'siteImagesPath' => '/images',
+	'imagesFolder'   => (YII_ENV_DEV ? 'http://localhost/workspace/carbnbnew/uploads/' : 'http://178.62.110.252/uploads/'),
+	'siteImagesPath' => (YII_ENV_DEV ? '/images' : 'http://178.62.110.252/frontend/web/images'),
 ];
