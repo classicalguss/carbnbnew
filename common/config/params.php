7695732a19f731a95d5e11ccc5a4bee4494d6ebe
Ghassan Barghouti <barghouti_since88@hotmail.com>
2017-09-29 15:17:35 +0300

    remove /web/ from URL

--- common/config/params.php
+++ common/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-	'imagesFolder'   => (YII_ENV_DEV ? 'http://localhost/workspace/carbnbnew/uploads/' : 'http://178.62.110.252/uploads/'),
-	'siteImagesPath' => (YII_ENV_DEV ? '/web/images' : 'http://178.62.110.252/frontend/web/images'),
+	'imagesFolder'   => 'http://178.62.110.252/uploads/',
+	'siteImagesPath' => '/images',
 	'apiDomain'=> (YII_ENV_DEV ? 'http://api.carbnb.com:8080' : 'http://api.bywai.info'),
 ];
