74d4ca9fae9a3ddc6166c1aefc282e0ff0e06124
Ghassan Barghouti <barghouti_since88@hotmail.com>
2019-02-09 14:36:26 +0000

    Some configuration changes

--- common/config/params.php
+++ common/config/params.php
@@ -3,9 +3,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-	'imagesFolder'   => 'http://178.62.110.252/uploads/',
+	'imagesFolder'   => 'http://139.59.129.21/uploads/',
 	'siteImagesPath' => '/images',
-	'apiDomain'=> (YII_ENV_DEV ? 'http://api.carbnb.com:8080' : 'http://api.bywai.info'),
+	'apiDomain'=> (YII_ENV_DEV ? 'http://api.carbnb.com:8080' : 'http://api.uchaise.com'),
 		'payfort'=>[
 				'merchant_identifier'=>'ULhiPMaP',
 				'access_code'=>'mjYoPHsRkzTGIlLPVvkX',
